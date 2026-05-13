6bbb4b3191d5e227b4822ddf308a5a0d506273eb
Steve Hamblett <steve.hamblett@linux.com>
2026-05-13 10:26:34 +0100

    Adder

---
+++ lib/adder/adder.cpp
@@ -0,0 +1,9 @@
+
+#include "adder.hpp"
+
+Adder::Adder() = default;
+
+int Adder::add(int a, int b)
+{
+    return a + b;
+}
---
+++ lib/adder/adder.hpp
@@ -0,0 +1,12 @@
+
+#pragma once
+
+class Adder
+{
+public:
+
+    Adder();
+
+    int add(int a, int b);
+
+};
